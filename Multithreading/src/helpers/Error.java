861ffb7db79c93485e56f825a970758d7e8afabf
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-09-11 08:12:01 +0700

    Create Error

---
+++ Multithreading/src/helpers/Error.java
@@ -0,0 +1,26 @@
+package helpers;
+
+public class Error {
+
+	public static void invail(String type) {
+		String name = null;
+		switch (type) {
+			case "integer":
+				name = "Số nguyên";
+				break;
+
+			case "answer":
+				name = "Câu trả lời";
+				break;
+
+			case "length":
+				name = "Chiều dài";
+				break;
+
+			case "width":
+				name = "Chiều rộng";
+				break;
+		}
+		System.err.println("LỖI: " + name + " không hợp lệ, vui lòng nhập lại!\n");
+	}
+}
