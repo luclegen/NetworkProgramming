c98bfb4f66bfd1fa6088a671bae102e9fd0e3364
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-09-20 09:46:56 +0700

    Create Notification, success, fail

---
+++ InetUrl/src/helpers/Notification.java
@@ -0,0 +1,35 @@
+package helpers;
+
+public class Notification {
+
+  public static void success(String type) {
+    String msg = null;
+    switch (type) {
+      case "url":
+        msg = "Bạn đã thêm 1 URL";
+        break;
+
+      case "history":
+        msg = "Bạn đã xoá lịch sử duyệt web";
+        break;
+
+      default:
+        break;
+    }
+    System.err.println("THÔNG BÁO: " + msg + ".\n");
+  }
+
+  public static void fail(String type, String spec) {
+    String msg = null;
+    switch (type) {
+      case "url":
+        msg = "Kết nối thất bại";
+        break;
+
+      default:
+        break;
+    }
+    System.err.println("CẢNH BÁO: " + msg + "!\n");
+  }
+
+}
