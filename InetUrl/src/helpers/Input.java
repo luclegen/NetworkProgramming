7e403951614dfbc9368272ec38ebd3d536e91cfb
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-09-20 09:45:11 +0700

    Create Input, setters, getters

---
+++ InetUrl/src/helpers/Input.java
@@ -0,0 +1,34 @@
+package helpers;
+
+import java.util.Scanner;
+
+public class Input {
+  private static Scanner sc = new Scanner(System.in);
+  private static String input = "";
+
+  // region SETTER
+  public static void setInput() {
+    input = sc.nextLine();
+  }
+
+  public static void setInput(String description) {
+    System.out.print(description);
+    input = sc.nextLine();
+  }
+  // endregion SETTER
+
+  // region GETTER
+
+  public static String getInput() {
+    setInput();
+    return input;
+  }
+
+  public static String getInput(String description) {
+    setInput(description);
+    return input;
+  }
+
+  // endregion GETTER
+
+}
