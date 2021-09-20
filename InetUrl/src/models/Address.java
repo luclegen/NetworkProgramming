3d05910c0458f76d74c13f67c7665122407b4676
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-09-20 09:44:34 +0700

    Create Address

---
+++ InetUrl/src/models/Address.java
@@ -0,0 +1,52 @@
+package models;
+
+import java.net.*;
+import java.time.LocalDateTime;
+
+import helpers.*;
+
+public class Address {
+
+  private URL url;
+  private String hostName, time, hostAddress;
+
+  public Address(String spec) {
+    url = Creator.url(spec);
+    hostName = url.getHost();
+    hostAddress = Creator.ip(hostName).getHostAddress();
+    time = VNDateTime.getDateShortTime(LocalDateTime.now());
+  }
+
+  public URL getUrl() {
+    return url;
+  }
+
+  public void setUrl(URL url) {
+    this.url = url;
+  }
+
+  public String getHostName() {
+    return hostName;
+  }
+
+  public void setHostName(String hostName) {
+    this.hostName = hostName;
+  }
+
+  public String getHostAddress() {
+    return hostAddress;
+  }
+
+  public void setHostAddress(String hostAddress) {
+    this.hostAddress = hostAddress;
+  }
+
+  public String getTime() {
+    return time;
+  }
+
+  @Override
+  public String toString() {
+    return url.toString() + "\t" + time;
+  }
+}
