597c9a2fcbc9722dabf585fa738c901eebb9ca65
Ngô Hoàng Thiện <thienngohoang99@gmail.com>
2021-09-20 10:18:13 +0700

    Test5

--- InetUrl/src/App.java
+++ InetUrl/src/App.java
@@ -79,7 +79,25 @@ public class App {
         break;
 
       case 5: // Bài tập 5
-
+        String host = hostname();
+        InetAddress inetAddress = InetAddress.getByName(host);
+        String hostAddress = "http://"+inetAddress.getHostAddress();
+        String ban1,ban2,ban3;
+        ban1 = "http://74.125.24.101";
+        ban2 = "http://104.21.95.31";
+        ban3 = "http://172.217.194.19";
+        while (hostAddress.matches(ban1) || hostAddress.matches(ban2) || hostAddress.matches(ban3)){
+          System.out.println("url is ban!");
+          hostAddress = hostname();
+        }
+        URL url = new URL(hostAddress);
+        URLConnection urlConnection = url.openConnection();
+        InputStream inputStream = urlConnection.getInputStream();
+        int i = inputStream.read();
+        while (i != -1) {
+          System.out.print((char) i);
+          i = inputStream.read();
+        }
         break;
 
       case 6: // Bài tập 6
@@ -94,5 +112,11 @@ public class App {
     System.out.print("\n\nĐã kết thúc bài tập " + index + ".");
     exLayout.footer(2);
   }
-
+  public static String hostname(){
+    String hostname;
+    Scanner input = new Scanner(System.in);
+    System.out.println("enter a hostname or IP  : ");
+    hostname = input.nextLine();
+    return hostname;
+  }
 }
