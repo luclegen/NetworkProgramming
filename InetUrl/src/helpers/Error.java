635936335027f902e88654f38ebf5175cd83511d
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-09-20 09:42:43 +0700

    Create column, url, ip

--- InetUrl/src/helpers/Error.java
+++ InetUrl/src/helpers/Error.java
@@ -2,17 +2,38 @@ package helpers;
 
 public class Error {
 
-	public static void invail(String type) {
-		String name = null;
+	public static void invalid(String type) {
+		String name = null, extension = ", vui lòng nhập lại!\n";
 		switch (type) {
+			case "column":
+				name = "Số cột";
+				break;
+
 			case "answer":
 				name = "Câu trả lời";
 				break;
 
+			case "url":
+				name = "Không thể kết nối hoặc địa chỉ URL";
+				break;
+
+			case "ip":
+				name = "Không thể kết nối hoặc địa chỉ IP";
+				break;
+
 			default:
-				name = "invail";
+				name = "invalid";
 				break;
 		}
-		System.err.println("LỖI: " + name + " không hợp lệ, vui lòng nhập lại!\n");
+		switch (type) {
+			case "url":
+			case "ip":
+				extension = ".\nVui lòng kiểm tra kết nối Internet và thử lại sau!\n";
+				break;
+
+			default:
+				break;
+		}
+		System.err.println("LỖI: " + name + " không hợp lệ" + extension);
 	}
 }
