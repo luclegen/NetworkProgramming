1e73a537e3ed04118fbb3bf982132aec45c98d44
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-09-21 16:36:55 +0700

    Create surf

--- InetUrl/src/helpers/Input.java
+++ InetUrl/src/helpers/Input.java
@@ -1,6 +1,9 @@
 package helpers;
 
+import java.net.*;
 import java.util.Scanner;
+
+import models.*;
 
 public class Input {
   private static Scanner sc = new Scanner(System.in);
@@ -31,4 +34,36 @@ public class Input {
 
   // endregion GETTER
 
+  // region NETWORK
+
+  public static String surf(String... banList) {
+    String result = null;
+    BanList list = new BanList(banList);
+    boolean isContinue = true;
+
+    do {
+      setInput("Địa chỉ web/IP hoặc hostname (\"exit\" để thoát): ");
+      if (input.equals("exit"))
+        break;
+
+      if (Checker.isHostAddress(input) || Checker.isHostname(input)) {
+        result = input;
+        isContinue = false;
+      } else {
+        try {
+          result = new URL(input).toString();
+          isContinue = false;
+        } catch (MalformedURLException e) {
+          Error.invalid("spec");
+          isContinue = true;
+        }
+      }
+      if (list.isBan(new HostAddress(input)))
+        Notification.ban("Trang web này");
+    } while (isContinue || list.isBan(new HostAddress(input)));
+
+    return result;
+  }
+
+  // endregion NETWORK
 }
