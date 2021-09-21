afbcfc8525ccd30743188643a457d8c143bc78d3
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-09-21 16:36:31 +0700

    Create HTML 2 show

---
+++ InetUrl/src/helpers/HTML.java
@@ -0,0 +1,47 @@
+package helpers;
+
+import java.io.*;
+import java.net.*;
+
+public class HTML {
+  private static String line;
+
+  @Deprecated
+  public static void show(URL url) {
+    if (url == null)
+      return;
+    try {
+      DataInputStream dis = new DataInputStream(url.openStream());
+      while ((line = dis.readLine()) != null)
+        System.out.println(line);
+    } catch (IOException e) {
+      return;
+    }
+  }
+
+  @Deprecated
+  public static void show(String spec) {
+    if (spec == null)
+      return;
+    BufferedReader reader = reader(Creator.URL(spec));
+    if (reader == null)
+      return;
+    try {
+      System.out.println();
+      while ((line = reader.readLine()) != null)
+        System.out.println(line);
+    } catch (IOException e) {
+      Error.invalid("host");
+    }
+  }
+
+  private static BufferedReader reader(URL url) {
+    if (url == null)
+      return null;
+    try {
+      return new BufferedReader(new InputStreamReader(url.openStream()));
+    } catch (IOException e) {
+      return null;
+    }
+  }
+}
