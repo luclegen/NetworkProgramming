8c7f1bd2e3ba325fc75152ce13cb86823d697e02
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-09-21 16:35:13 +0700

    Create hostToURL, URL

--- InetUrl/src/helpers/Creator.java
+++ InetUrl/src/helpers/Creator.java
@@ -3,12 +3,52 @@ package helpers;
 import java.net.*;
 
 public class Creator {
-
-  public static URL url(String uri) {
+  public static URL URL(String spec) {
     try {
-      return new URL(uri);
+      return new URL(spec);
     } catch (MalformedURLException e) {
-      Error.invalid("url");
+      if (Checker.isHostAddress(spec) || Checker.isHostname(spec))
+        return hostToURL(spec);
+      else {
+        Error.invalid("spec");
+        return null;
+      }
+    }
+  }
+
+  public static URL hostToURL(String host) {
+    String type = Checker.isHostAddress(host) ? "ip" : Checker.isHostname(host) ? "hostname" : "host";
+
+    try {
+      String protocol = "http";
+      URL url = null;
+
+      if (Checker.isHostAddress(host)) {
+        if (!Checker.isSuccessConnection(new URL(protocol, host, "/")))
+          protocol += "s";
+
+        if (!Checker.isSuccessConnection(new URL(protocol, host, "/"))) {
+          try {
+            host = InetAddress.getByName(host).getHostName();
+          } catch (UnknownHostException e) {
+            Error.invalid(type);
+            return null;
+          }
+        }
+
+        url = new URL(protocol, host, "/");
+      }
+
+      if (Checker.isHostname(host)) {
+        if (!Checker.isSuccessConnection(new URL(protocol, host, "/")))
+          protocol += "s";
+
+        url = new URL(protocol, host, "/");
+      }
+
+      return url == null ? null : Checker.isSuccessConnection(url) ? url : null;
+    } catch (MalformedURLException e) {
+      Error.invalid(type);
       return null;
     }
   }
