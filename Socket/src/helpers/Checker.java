46c075fc84e8f5f029f0bf002e3acf5e47b4620e
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-10-14 10:01:36 +0700

    Create Checker

---
+++ Socket/src/helpers/Checker.java
@@ -0,0 +1,30 @@
+package helpers;
+
+public class Checker {
+
+  // region INPUT
+
+  public static boolean isContinue() {
+    while (true) {
+      System.out.println("\nBạn có muốn tiếp tục? (Y/n)");
+      switch (Input.getInput("Trả lời: ")) {
+        case "":
+        case "Y":
+        case "y":
+          System.out.println();
+          return true;
+
+        case "n":
+        case "N":
+          System.out.println();
+          return false;
+
+        default:
+          Error.invalid("answer");
+          break;
+      }
+    }
+  }
+
+  // endregion INPUT
+}
