600f1435f1ce186b1ac842bec74c88792f2b7ea7
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-10-14 09:41:15 +0700

    Update App

--- Socket/src/App.java
+++ Socket/src/App.java
@@ -1,4 +1,3 @@
-import apps.*;
 import clis.*;
 import helpers.*;
 import helpers.Error;
@@ -10,39 +9,28 @@ public class App {
   public static void main(String[] args) throws Exception {
     layout.header(1);
 
+    char choice = '0';
     do {
-      char choice = '0';
-      do {
-        System.out.println("Hãy chọn số bài tập mà bạn cần thực hiện? (1 -> 4 | 0: Để thoát chương trình)");
-        choice = Input.getChar("Trả lời: ");
+      System.out.println("Hãy chọn số bài tập mà bạn cần thực hiện? (1 -> 4 | 0: Để thoát chương trình)");
+      choice = Input.getChar("Trả lời: ");
 
-        switch (choice) {
-          case '0':
-            System.out.print("\nĐã thoát chương trình.");
-            layout.footer(1);
-            System.exit(0);
-            break;
+      switch (choice) {
+        case '0':
+          System.out.print("\nĐã thoát chương trình.");
+          layout.footer(1);
+          System.exit(0);
+          break;
 
-          case '1':
-            App1.run();
-            break;
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+          System.out.println("Nằm ở gói: " + "Socket/src/apps/" + choice + ".\n");
+          break;
 
-          case '2':
-            App2.run();
-            break;
-
-          case '3':
-            App3.run();
-            break;
-
-          case '4':
-            App4.run();
-            break;
-
-          default:
-            Error.invalid("answer");
-        }
-      } while (true);
+        default:
+          Error.invalid("answer");
+      }
     } while (true);
   }
 
