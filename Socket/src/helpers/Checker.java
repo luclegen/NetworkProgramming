5a79c31e38b472dea3d4c3dc936cc434f789e811
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-10-18 22:05:52 +0700

    Create isHostname and isHostAddress

--- Socket/src/helpers/Checker.java
+++ Socket/src/helpers/Checker.java
@@ -27,4 +27,16 @@ public class Checker {
   }
 
   // endregion INPUT
+
+  // #region NETWORK
+
+  public static boolean isHostname(String host) {
+    return host.matches("^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}$");
+  }
+
+  public static boolean isHostAddress(String host) {
+    return host.matches("^((25[0-5]|2[0-4][0-9]|[1]?[1-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[1]?[1-9][0-9]?)$");
+  }
+
+  // #endregion NETWORK
 }
