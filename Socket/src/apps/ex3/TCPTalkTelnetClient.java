886522dc02819fec0c63f7939c2b67f39defb94f
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-10-25 08:43:13 +0700

    Inherit TalkController

--- Socket/src/apps/ex3/TCPTalkTelnetClient.java
+++ Socket/src/apps/ex3/TCPTalkTelnetClient.java
@@ -3,27 +3,20 @@ package apps.ex3;
 import java.io.*;
 import java.net.Socket;
 
-import java.awt.event.*;
-
-import javax.swing.ImageIcon;
-import javax.swing.event.*;
-
 import clis.Layout;
+import controllers.TalkController;
 import helpers.*;
 import models.Message;
 import views.Talk;
 
-public class TCPTalkTelnetClient {
+public class TCPTalkTelnetClient extends TalkController {
   private static Layout layout = new Layout(72, "CLIENT");
-  private Message model;
-  private Talk view;
   private static Socket socket = null;
   private static BufferedWriter out = null;
   private static BufferedReader in = null;
 
   public TCPTalkTelnetClient(Message model, Talk view) {
-    this.model = model;
-    this.view = view;
+    super(model, view);
     try {
       out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
     } catch (IOException e) {
@@ -34,97 +27,21 @@ public class TCPTalkTelnetClient {
     } catch (IOException e) {
       System.out.println("Không thể tạo đầu đọc: " + e);
     }
-    initController();
   }
 
-  private void initController() {
-    view.addComponentListener(resize());
-    view.getConversationScrollPane().getViewport().addChangeListener(fit());
-    view.getMessageTextField().getDocument().addDocumentListener(check());
-    view.getMessageTextField().addActionListener(e -> send());
-    view.getSendButton().addActionListener(e -> send());
-  }
-
-  private ComponentListener resize() {
-    return new ComponentListener() {
-
-      @Override
-      public void componentHidden(ComponentEvent e) {
-      }
-
-      @Override
-      public void componentMoved(ComponentEvent e) {
-      }
-
-      @Override
-      public void componentResized(ComponentEvent e) {
-        view.getMainSplitPane().setDividerLocation(view.getHeight() - 90);
-        view.getMessageTextField().setColumns((view.getWidth() - view.getSendButton().getWidth() - 30) / 30);
-      }
-
-      @Override
-      public void componentShown(ComponentEvent e) {
-      }
-    };
-  }
-
-  private DocumentListener check() {
-    return new DocumentListener() {
-      public void check() {
-        view.getSendButton().setIcon(new ImageIcon(System.getProperty("user.dir") + "/src/images/"
-            + (view.getMessageTextField().getText().length() > 0 ? "" : "inactive-") + "send.png"));
-      }
-
-      @Override
-      public void changedUpdate(DocumentEvent e) {
-        check();
-      }
-
-      @Override
-      public void insertUpdate(DocumentEvent e) {
-        check();
-      }
-
-      @Override
-      public void removeUpdate(DocumentEvent e) {
-        check();
-      }
-    };
-  }
-
-  public void send() {
-    String content = view.getMessageTextField().getText();
-    if (content.length() > 0) {
+  public boolean send() {
+    boolean isSend = super.send();
+    if (isSend)
       try {
-        model.setContent(content);
-        out.write(model.getName());
+        out.write(getModel().getName());
         out.newLine();
-        out.write(model.getContent());
+        out.write(getModel().getContent());
         out.newLine();
         out.flush();
-        view.addSentMessage(model);
-        view.getMessageTextField().setText("");
       } catch (IOException e) {
         System.err.println("Không thể gửi tin nhắn: " + e);
       }
-    }
-  }
-
-  private ChangeListener fit() {
-    return new ChangeListener() {
-      @Override
-      public void stateChanged(ChangeEvent e) {
-        view.getConversationPanel()
-            .setSize(view.getWidth() - (view.getConversationScrollPane().getVerticalScrollBar().isVisible()
-                ? view.getConversationScrollPane().getVerticalScrollBar().getWidth()
-                : 0) - 5, view.getConversationPanel().getHeight());
-      }
-    };
-  }
-
-  public void receive(Message message) {
-    if (message.getContent().length() > 0)
-      view.addReceivedMessage(message);
+    return isSend;
   }
 
   public static void main(String[] args) {
@@ -134,9 +51,8 @@ public class TCPTalkTelnetClient {
       System.out.println("Đã kết nối tới: " + socket);
       TCPTalkTelnetClient client = new TCPTalkTelnetClient(new Message(Input.getInput("Hãy nhập tên bạn: ")),
           new Talk("Client", Talk.RIGHT));
-      while (true) {
+      while (true)
         client.receive(new Message(in.readLine(), in.readLine()));
-      }
     } catch (IOException e) {
       System.err.println("Không thể kết nối tới server.");
     } finally {
