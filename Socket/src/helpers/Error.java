444dd8a4f27a847ec6177335f50169d410bc3556
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-10-25 09:53:20 +0700

    Create reason, type, msg, username, password, char

--- Socket/src/helpers/Error.java
+++ Socket/src/helpers/Error.java
@@ -3,36 +3,65 @@ package helpers;
 public class Error {
 
 	public static void invalid(String type) {
-		String name = null, extension = ", vui lòng nhập lại!\n";
+		String name = null, reason = " không hợp lệ", extension = ", vui lòng nhập lại!\n";
 		switch (type) {
-			case "operation":
-				name = "Phép tính";
-				break;
+		case "operation":
+			name = "Phép tính";
+			break;
 
-			case "answer":
-				name = "Câu trả lời";
-				break;
+		case "answer":
+			name = "Câu trả lời";
+			break;
 
-			case "hostname":
-				name = "Không thể kết nối hoặc hostname";
-				break;
+		case "type":
+			name = "Loại";
+			break;
 
-			case "hostAddress":
-				name = "Không thể kết nối hoặc địa chỉ IP";
-				break;
+		case "msg":
+			name = "Tin nhắn";
+			break;
 
-			default:
-				break;
+		case "username":
+			name = "Tên người dùng";
+			break;
+
+		case "password":
+			name = "Mật khẩu";
+			break;
+
+		case "hostname":
+			name = "Không thể kết nối hoặc hostname";
+			break;
+
+		case "hostAddress":
+			name = "Không thể kết nối hoặc địa chỉ IP";
+			break;
+
+		case "char":
+			name = "Ký tự";
+			break;
+
+		default:
+			name = "Tên lỗi";
+			break;
 		}
 		switch (type) {
-			case "hostname":
-			case "hostAddress":
-				extension = ".\nVui lòng kiểm tra kết nối Internet và thử lại sau!\n";
-				break;
+		case "password":
+			reason = " chứa ít nhất 8 ký tự";
+			break;
 
-			default:
-				break;
+		default:
+			break;
 		}
-		System.err.println("LỖI: " + name + " không hợp lệ" + extension);
+		switch (type) {
+		case "hostname":
+		case "hostAddress":
+			extension = ".\nVui lòng kiểm tra kết nối Internet và thử lại sau!\n";
+			break;
+
+		default:
+			break;
+		}
+		System.err.println("LỖI: " + name + reason + extension);
 	}
 }
