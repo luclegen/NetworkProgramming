499e6a9a96392f976e72bd42da41df0d8a29c339
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-12-16 11:47:18 +0700

    Add isContinue

--- UDPSocket/src/helpers/Checker.java
+++ UDPSocket/src/helpers/Checker.java
@@ -2,6 +2,32 @@ package helpers;
 
 public abstract class Checker {
 
+  // region INPUT
+
+  public static boolean isContinue() {
+    while (true) {
+      System.out.println("\nBạn có muốn tiếp tục? (Y/n)");
+      switch (Input.getInput("Trả lời: ")) {
+        case "":
+        case "Y":
+        case "y":
+          System.out.println();
+          return true;
+
+        case "n":
+        case "N":
+          System.out.println();
+          return false;
+
+        default:
+          Error.invalid("answer");
+          break;
+      }
+    }
+  }
+
+  // endregion INPUT
+
   // #region CHARACTER
 
   public static boolean isChar(String input) {
