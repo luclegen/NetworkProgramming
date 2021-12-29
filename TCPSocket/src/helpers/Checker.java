c52e8166d6ec557b599594cdbd4e1d2e85f1c8f0
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-12-29 10:54:48 +0700

    Create isHostname and isHostAddress

--- TCPSocket/src/helpers/Checker.java
+++ TCPSocket/src/helpers/Checker.java
@@ -44,4 +44,16 @@ public abstract class Checker {
 
   // #endregion NUMBER
 
+  // #region NETWORK
+
+  public static boolean isHostname(String host) {
+    return host.matches("^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}$");
+  }
+
+  public static boolean isHostAddress(String host) {
+    return host.matches("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+  }
+
+  // #endregion NETWORK
+
 }
