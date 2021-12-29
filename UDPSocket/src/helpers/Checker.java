c810016d20dcfc3fd21e7760e2093da9320aae5a
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-12-29 16:18:31 +0700

    Create Checker

--- UDPSocket/src/helpers/Checker.java
+++ UDPSocket/src/helpers/Checker.java
@@ -36,4 +36,32 @@ public abstract class Checker {
 
   // #endregion CHARACTER
 
+  // #region STRING
+
+  public static boolean isUsername(String input) {
+    return input.matches("^[a-zA-Z0-9._-]+$");
+  }
+
+  // #endregion STRING
+
+  // #region NUMBER
+
+  public static boolean isInteger(String input) {
+    return input.matches("^[-+]?\\d+");
+  }
+
+  // #endregion NUMBER
+
+  // #region NETWORK
+
+  public static boolean isHostname(String host) {
+    return host.matches("^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}$");
+  }
+
+  public static boolean isHostAddress(String host) {
+    return host.matches("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+  }
+
+  // #endregion NETWORK
+
 }
