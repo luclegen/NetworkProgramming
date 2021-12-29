edd8a1fd98ae4b63b752336859c6416b94e1a487
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-12-29 16:18:48 +0700

    Create Error

--- UDPSocket/src/helpers/Error.java
+++ UDPSocket/src/helpers/Error.java
@@ -5,6 +5,10 @@ public abstract class Error {
   public static void invalid(String type) {
     String name = null, reason = " không hợp lệ", extension = ", vui lòng nhập lại!\n";
     switch (type) {
+      case "input":
+        name = "Đầu vào";
+        break;
+
       case "answer":
         name = "Câu trả lời";
         break;
@@ -17,8 +21,32 @@ public abstract class Error {
         name = "Phép toán";
         break;
 
+      case "hostAddress":
+        name = "Không thể kết nối hoặc địa chỉ IP";
+        break;
+
+      case "password":
+        name = "Mật khẩu";
+        break;
+
       default:
-        name = "Loại lỗi";
+        name = "Tên lỗi";
+        break;
+    }
+    switch (type) {
+      case "password":
+        reason = " chứa ít nhất 8 ký tự";
+        break;
+
+      default:
+        break;
+    }
+    switch (type) {
+      case "hostAddress":
+        extension = ".\nVui lòng kiểm tra kết nối Internet và thử lại sau!\n";
+        break;
+
+      default:
         break;
     }
     System.err.println("LỖI: " + name + reason + extension);
