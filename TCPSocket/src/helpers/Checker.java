ac774184db51d27e2dd255973ca6ae7275a97640
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-12-29 09:52:01 +0700

    Add isContinue and isInteger

--- TCPSocket/src/helpers/Checker.java
+++ TCPSocket/src/helpers/Checker.java
@@ -2,6 +2,32 @@ package helpers;
 
 public abstract class Checker {
 
+  // region INPUT
+
+  public static boolean isContinue() {
+    while (true) {
+      System.out.println("\nBạn có muốn tiếp tục? (Y/n)");
+      switch (Input.getInput("Trả lời: ")) {
+        case "":
+        case "Y":
+        case "y":
+          System.out.println();
+          return true;
+
+        case "n":
+        case "N":
+          System.out.println();
+          return false;
+
+        default:
+          Error.invalid("answer");
+          break;
+      }
+    }
+  }
+
+  // endregion INPUT
+
   // #region CHARACTER
 
   public static boolean isChar(String input) {
@@ -9,5 +35,13 @@ public abstract class Checker {
   }
 
   // #endregion CHARACTER
+
+  // #region NUMBER
+
+  public static boolean isInteger(String input) {
+    return input.matches("^[-+]?\\d+");
+  }
+
+  // #endregion NUMBER
 
 }
