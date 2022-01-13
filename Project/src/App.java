cfac5e97e64969af4e4ac13ea0700580cbe12f24
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-13 08:16:47 +0700

    Update Project

--- Project/src/App.java
+++ Project/src/App.java
@@ -12,24 +12,16 @@ public class App {
 	public static void main(String[] args) throws Exception {
 		List<User> users = new ArrayList<>();
 
-		System.out.println("THIẾT LẬP TÀI KHOẢN ADMIN");
-		// System.out.println("Tên : ");
-		// System.out.println("Họ : ");
-		// System.out.println("Tên người dùng: ");
-		// System.out.println("Mật khẩu : ");
-		// System.out.println("Xác nhận : ");
-		// User admin = new User(
-		// Input.getInput(
-		// "Tên : "),
-		// Input.getInput(
-		// "Họ : "),
-		// Input.getUsername(users,
-		// "Tên người dùng: "),
-		// Input.getPassword("Mật khẩu : ", "Xác nhận : "), Type.Admin);
-		User admin = new User("Lực", "Huỳnh Tấn", "luc", "Luc123!@#", Type.Admin);
+		User admin = new User(
+				Input.getInput(
+						"Tên           : "),
+				Input.getInput(
+						"Họ            : "),
+				Input.getUsername(users,
+						"Tên người dùng: "),
+				Input.getPassword("Mật khẩu      : ", "Xác nhận      : "), Type.Admin);
 		UserDAO.create(admin);
 
 		new LoginController(new User(), new LoginView());
-		// System.out.println(user);
 	}
 }
